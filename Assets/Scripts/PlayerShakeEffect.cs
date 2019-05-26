f2787a0a5ae96884363f32697409d53fa5e6b34e
vashgard <diegoalfonso_1991@yahoo.es>
2019-05-26 21:39:09 +0200

    Ampliación del "Efecto caida"
    ajustados los parametros de balanceo

---
+++ Assets/Scripts/PlayerShakeEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShakeEffect : MonoBehaviour
+{
+    // vars
+    public bool shakeOn = true;
+    public float horizontalShakeAmplitude;
+    public float verticarlShakeAmplitude;
+
+    // sprite original position
+    private Vector2 originPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // if shake is enabled
+        if (shakeOn)
+        {
+            originPosition = this.transform.position;
+            transform.Find("Sprite").transform.position = originPosition + new Vector2(Mathf.Sin(Time.time) * horizontalShakeAmplitude, Mathf.Sin(Time.time * 2) * verticarlShakeAmplitude);
+        }
+    }
+
+    
+
+}
