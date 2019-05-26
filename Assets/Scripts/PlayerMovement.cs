ee1438a898304e9e031814b77d2cc1e71d087185
vashgard <diegoalfonso_1991@yahoo.es>
2019-05-26 21:02:21 +0200

    Añadido movimiento

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // MOVEMENT
+    private float currentSpeed = 0.0f;
+    public float playerSpeed = 4.0f;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    //Mover el personaje
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rigidbody.velocity = movement * playerSpeed;
+
+        //Limita el movimiento dentro de la camara principal
+        var pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
+        pos.y = Mathf.Clamp(pos.y, 0.1f, 0.9f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+
+    }
+}
