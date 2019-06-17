d704f300d92328aa76aa4f42aac75c2a663e1674
vashgard <50802010+vashgard@users.noreply.github.com>
2019-06-17 12:07:49 +0200

    Las almas se destruyen en contacto con el jugador

--- Assets/Scripts/InFallObjects/EnemyMovementInFall.cs
+++ Assets/Scripts/InFallObjects/EnemyMovementInFall.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMovementInFall : MonoBehaviour
 {
     // MOVEMENT
     public float enemySpeed = 4.0f;
